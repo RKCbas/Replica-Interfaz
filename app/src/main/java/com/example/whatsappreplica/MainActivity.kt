1d21f8d536624d96300ce2ff58b0f2143787efa1
RKCbas <sebashp2015@hotmail.com>
2024-09-19 11:07:18 -0600

    disabeled edge to edge

--- app/src/main/java/com/example/whatsappreplica/MainActivity.kt
+++ app/src/main/java/com/example/whatsappreplica/MainActivity.kt
@@ -51,7 +51,7 @@ import com.example.whatsappreplica.ui.theme.WhatsAppReplicaTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
 
         setContent{
             ComposeMultiScreenApp()
